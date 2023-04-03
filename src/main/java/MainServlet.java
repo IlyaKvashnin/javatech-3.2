4b5442ffa4b628421b23c568b9e761814936ea4b
ilya <27romashka05@gmail.com>
2023-04-03 21:37:42 +0500

    Задеплоил томкат с одной пустой главной страничкой

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MainServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.setAttribute("name", ".......");
+
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+
+    }
+
 }
--- src/main/webapp/mypage.jsp
+++ src/main/webapp/mypage.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: ilyakvashnin
-  Date: 03.04.2023
-  Time: 21:23
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Main</title>
+</head>
+<body>
+<h1>Hello ${name}</h1>
+</body>
 </html>
