973a41516c77b376c7dbe6b04cf940b27d4be29f
ilya <27romashka05@gmail.com>
2023-04-09 15:48:23 +0500

    Добавил вывод времени и немного разметки

---
+++ src/main/java/com/example/javatech/MainServlet.java
@@ -0,0 +1,69 @@
+package com.example.javatech;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/files")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        req.setAttribute("dateTime", dtf.format(now));
+
+        String path = req.getParameter("path");
+        if (path == null) {
+            path = System.getProperty("user.dir");
+            resp.sendRedirect(String.format("%s%s?path=%s", req.getContextPath(), req.getServletPath(), URLEncoder.encode(path, StandardCharsets.UTF_8.toString())));
+            return;
+        }
+
+        setContentFolders(req,path);
+
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+
+    }
+
+    private void setContentFolders(HttpServletRequest req, String path) {
+        File file = new File(path);
+        File[] allFiles = file.listFiles();
+
+        if (allFiles != null) {
+            List<File> directories = new ArrayList<>();
+            List<File> files = new ArrayList<>();
+
+            for (File f : allFiles) {
+                if (f.getPath() != null) {
+                    if (f.isDirectory())
+                        directories.add(f);
+                    else
+                        files.add(f);
+                }
+            }
+
+            req.setAttribute("directories", directories);
+            req.setAttribute("files", files);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("POST method isn't available");
+    }
+}
--- src/main/webapp/mypage.jsp
+++ src/main/webapp/mypage.jsp
@@ -1,9 +1,37 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" import="java.nio.file.Files,java.io.File" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Main</title>
 </head>
 <body>
-<h1>Hello ${name}</h1>
+<p>${dateTime}</p>
+
+<table>
+    <thead>
+    <tr>
+        <th>File</th>
+        <th>Size</th>
+        <th>Date</th>
+    </tr>
+    </thead>
+    <tbody>
+    <form action="./files" method="get">
+        <c:forEach var="directory" items ="${directories}">
+            <tr>
+                <td>
+                    <button type="submit" name="path" value="${directory.getAbsolutePath()}">
+                        <div>${directory.getName()}/</div>
+                    </button>
+                </td>
+                <td></td>
+                <td>
+                    <div>${Files.getAttribute(directory.toPath(),"lastModifedTime").toString()}</div>
+                </td>
+            </tr>
+        </c:forEach>
+    </form>
+    </tbody>
+</table>
 </body>
 </html>
